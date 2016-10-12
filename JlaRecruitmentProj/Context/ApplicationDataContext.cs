55f85e11eec458f13bb35bf2ef425f4281c5d3e0
Michael Karamanolis <michaelkara@mweb.co.za>
2016-10-12 19:41:33 +0200

    new

---
+++ JlaRecruitmentProj/Context/ApplicationDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using JlaRecruitmentProj.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace JlaRecruitmentProj.Context
+{
+    public class ApplicationDataContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDataContext()
+            : base("DefaultConnection")
+        { }
+
+        public System.Data.Entity.DbSet<AppUser> AppUsers { get; set; }
+    }
+}
---
+++ JlaRecruitmentProj/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JlaRecruitmentProj.Startup))]
+namespace JlaRecruitmentProj
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
